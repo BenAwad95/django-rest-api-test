343158b105e2de81b33f035114d99181125736d3
Abdullah Awad <abdullah.alashafi@gmail.com>
2022-04-27 23:43:49 +0300

    collect all files

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,11 @@
 import requests
 
-endpint = 'https://httpbin.org/anything'
+endpint1 = 'https://httpbin.org/anything'
+endpint2 = 'http://localhost:8000/'
 
-get_res = requests.get(endpint, json={'name': 'Abdullah', 'age': 26})
+get_res = requests.get(endpint2, json={'name': 'Abdullah', 'age': 26})
 
-# print(get_res.text)
-# print(get_res.json)
+print(get_res.text)
+print(get_res.json)
+print(get_res.status_code)
 
